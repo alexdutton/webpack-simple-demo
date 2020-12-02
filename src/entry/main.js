6ad10287da8c74030e725d21cbc05007e495bc67
Alex Dutton <alex@cottagelabs.com>
2020-12-02 10:55:47 +0000

    Initial commit

---
+++ src/entry/main.js
@@ -0,0 +1,5 @@
+import './main.scss';
+
+document.addEventListener("load", event => {
+    console.log("Hello World!");
+})
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+var path = require('path');
+var webpack = require('webpack');
+var BundleTracker = require('webpack-bundle-tracker');
+
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+
+    entry: {
+        main: './entry/main.js',
+        roCrate: './entry/roCrate.js',
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "[name]-[hash].js",
+        publicPath: "/static/bundles/"
+
+    },
+
+    module: {
+
+        rules: [
+            {
+                test: /.(scss|css)$/,
+
+                use: [{
+                    loader: MiniCssExtractPlugin.loader
+                }, {
+                    loader: "css-loader",
+
+                    options: {
+                        sourceMap: true
+                    }
+                }, {
+                    loader: "sass-loader",
+
+                    options: {
+                        sourceMap: true
+                    }
+                }]
+            },
+            {
+                test: /.js$/,
+                use: []
+            }
+        ]
+    },
+    plugins: [
+        new BundleTracker({filename: './webpack-stats.json'}),
+        new MiniCssExtractPlugin({ filename:'[name]-[chunkhash].css' })
+    ],
+    watch: true
+}
